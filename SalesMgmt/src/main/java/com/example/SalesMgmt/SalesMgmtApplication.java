89ec9bfbc734733f08c8fd0b6f08a6a0859ee7a2
Reethujanani <90744967+Reethujanani@users.noreply.github.com>
2021-10-30 12:25:36 +0530

    Add files via upload

---
+++ SalesMgmt/src/main/java/com/example/SalesMgmt/SalesMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.SalesMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesMgmtApplication.class, args);
+	}
+
+}
